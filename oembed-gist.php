8cee1a1a58e473c96e94842fa0d733c011722da9
Takayuki Miyauchi <miya@theta.ne.jp>
2012-04-21 18:31:54 +0900

    first commit

---
+++ oembed-gist.php
@@ -0,0 +1,90 @@
+<?php
+/*
+Plugin Name: oEmbed Gist
+Plugin URI: http://firegoby.jp/wp/oembed-gist
+Description: Embed source from gist.github.
+Author: Takayuki Miyauchi
+Version: 1.3.0
+Author URI: http://firegoby.jp/
+*/
+
+/*
+Copyright (c) 2010 Takayuki Miyauchi (THETA NETWORKS Co,.Ltd).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Modified August 18, 2011 by Alex King (alexking.org) to add NOSCRIPT and i18n support
+
+*/
+
+new gist();
+
+class gist {
+
+private $noscript;
+private $html = '<script src="https://gist.github.com/%s.js%s"></script><noscript>%s</noscript>';
+
+function __construct()
+{
+    add_action('plugins_loaded', array(&$this, 'plugins_loaded'));
+}
+
+public function plugins_loaded()
+{
+    load_plugin_textdomain(
+        'oembed-gist',
+        false,
+        dirname(plugin_basename(__FILE__)).'/languages'
+    );
+
+    wp_embed_register_handler(
+        'gist',
+        '#https://gist.github.com/([a-zA-Z0-9]+)(\#file_(.+))?$#i',
+        array(&$this, 'handler')
+    );
+    add_shortcode('gist', array(&$this, 'shortcode'));
+}
+
+public function handler($m, $attr, $url, $rattr)
+{
+    if (!isset($m[2]) || !isset($m[3]) || !$m[3]) {
+        $m[3] = null;
+    }
+    return '[gist id="'.$m[1].'" file="'.$m[3].'"]';
+}
+
+public function shortcode($p)
+{
+    if (preg_match("/^[a-zA-Z0-9]+$/", $p['id'])) {
+        $noscript = sprintf(
+            __('<p>View the code on <a href="https://gist.github.com/%s">Gist</a>.</p>', 'oembed-gist'),
+            $p['id']
+        );
+        if ($p['file']) {
+            return sprintf($this->html, $p['id'], '?file='.$p['file'], $noscript);
+        } else {
+            return sprintf($this->html, $p['id'], '', $noscript);
+        }
+    }
+}
+
+}
+
+
+// EOF
---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== oEmbed Gist ===
+Contributors: miyauchi
+Donate link: https://www.paypal.com/jp/cgi-bin/webscr?cmd=_flow&SESSION=V9ggRQYdPscPXHHzwIMAG2o2RavMQO8KLdFTxHm3dzuX2gDiDHw-k1ZBw5C&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b0819838956b846fa597911f234fb937456ea7
+Tags: url, link, oembed, visual editor, post, gist, github
+Requires at least: 3.0
+Tested up to: 3.3.2
+Stable tag: 1.3.0
+
+Embed your source from gist into WordPress easily.
+
+== Description ==
+
+* Embed your source from gist into WordPress easily.
+* You simply paste URL of your Gist.
+
+= Summary =
+
+You can embed your program sources from gist simply paste URL like below.
+
+* https://gist.github.com/973153
+* https://gist.github.com/973153#file_select_media.html
+
+PHP5 is required!
+
+= Support =
+
+* [Plugin Homepage](http://firegoby.theta.ne.jp/wp/oembed-gist)
+* [Support](http://wordpress.org/tags/oembed-gist)
+
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+
+== Screenshots ==
+
+1. Copy URL from gist.github.com.
+1. You simply paste URL into WordPress.
+1. It just appears!
+
+== Changelog ==
+
+= 1.2.0 =
+* Fixed to specification changes of Gist.
+
+= 1.1.0 =
+* Add support NOSCRIPT
+
+= 0.1.0 =
+* The first release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
+== Contact ==
+
+* email to miya[at]theta.ne.jp
+* twitter @miya0001
